5f0c009e000a0ee502c4040a754d45c6d620a43b
baopham101 <baopham101@gmail.com>
2020-07-10 08:24:53 +0700

    1st: the first upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Welcome to baopham repository
+This repo contains the source code of a simple java application used to demonstrate the CI - Continuous Integration process - a part of Introduction to Software Engineering course at FPT university - HCMC Campus
+
+
+## You will find this repo:
+* MathUtil class - holds the list of math methods
+* Test class - demonstrate on Unit Testing and TDD
+* .gitignore
+* Markdown file - this file itself
+
+## Connect via me
+1. [GitHub](https://github.com/baopham101)
+2. [Facebook](http://facebook.com)
+
+
+#### © 2020 by baopham
---
+++ src/bao/util/MathUtil.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bao.util;
+
+/**
+ *
+ * @author Admin
+ */
+//đây là classs chứa các hàm toán học, tính toán, với mục tiêu xài chung
+//cho các nơi khác, phàm cái gì mang tính chất xài chung, a sẽ dùng STATIC
+public class MathUtil {
+
+    //tính n! = 1.2.3...n. Vì n! tăng giá trị rất nhanh, sớm tràn miền int
+    //int chỉ tối đa 2 tỷ 1, nên ta cài long mới an tâm
+    //thực ra 15! to lắm, nên ta chỉ tính giai thừa từ 15 trở lại
+    public static long computeFactorial(int n) {
+        
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+}

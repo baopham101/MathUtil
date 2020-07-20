9ddb668d21a58c2ef675bedd5a3ace0edc393704
baopham101 <baopham101@gmail.com>
2020-07-20 08:11:04 +0700

    9th: correct the logic falacy

--- test/tdd/MathUtilTest.java
+++ test/tdd/MathUtilTest.java
@@ -49,7 +49,7 @@ public class MathUtilTest {
         //vì hàm này cần 2 value expected và actual
         //trong khi đó ném ra ngoại lệ không phải là actual value để ss
         //ta phải xài chiêu khác khi bắt ngoại lệ coi có đúng là ngoại lệ mình cần ko
-        computeFactorial(-5);
+        //computeFactorial(-5);
         computeFactorial(16); //giai thừa tăng rất nhanh, không chơi quá 15
     }
 
